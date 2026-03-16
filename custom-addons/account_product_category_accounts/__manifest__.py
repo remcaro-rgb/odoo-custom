45acaab0045262c15993490e0efd8cd203280a0c
remcaro-rgb <remcaro@gmail.com>
2026-03-16 13:22:17 -0500

    feat(account_product_category_accounts): add module scaffold

---
+++ custom-addons/account_product_category_accounts/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Account Product Category Accounts',
+    'version': '18.0.1.0.0',
+    'summary': 'Show income/expense accounts on product categories for all internal users',
+    'category': 'Accounting',
+    'depends': ['account', 'product'],
+    'data': ['views/product_category_views.xml'],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
